073f85e8600cfa8ffc90038eefb7c1fb050fcc76
fire1fly <danyseidev@gmail.com>
2023-04-26 00:10:07 +0800

    feat: get files

--- src/files/files.module.ts
+++ src/files/files.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { FilesController } from './files.controller';
+import { FileEntity } from './entities/file.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   controllers: [FilesController],
-  providers: [FilesService]
+  providers: [FilesService],
+  imports: [TypeOrmModule.forFeature([FileEntity])]
 })
 export class FilesModule {}
